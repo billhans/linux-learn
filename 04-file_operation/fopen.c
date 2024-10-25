e8211b18392bf4aa32c494ecdf5d0b7a3ddfc859
bhhh <3343806536@qq.com>
2024-10-26 00:38:21 +0800

    change mode

--- 04-file_operation/fopen.c
+++ 04-file_operation/fopen.c
@@ -2,7 +2,13 @@
 
 int main(int argc, char *argv[]) {
     ARGS_CHECK(argc, 2);
-    FILE *fp = fopen(argv[1], "r");
+    // FILE *fp = fopen(argv[1], "a");
+    // ERROR_CHECK(fp, NULL, "fopen");
+    // fwrite("howareyou", 1, 9, fp);
+    FILE *fp = fopen(argv[1], "a+");
     ERROR_CHECK(fp, NULL, "fopen");
+    char buf[10] = {0};
+    fread(buf, 1, 9, fp);
+    fwrite("howareyou", 1, 9, fp);
     fclose(fp);
 }
