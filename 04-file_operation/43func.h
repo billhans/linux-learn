28aad262982a07056df6d39ba8f3a5830831043c
bhhh <3343806536@qq.com>
2024-10-26 15:17:42 +0800

    update 43func.h

--- 04-file_operation/43func.h
+++ 04-file_operation/43func.h
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #define ARGS_CHECK(argc, num)                 \
     {                                         \
