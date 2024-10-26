8af2336478eb6d3dd489a4af1102252672e4dd93
bhhh <3343806536@qq.com>
2024-10-26 15:16:36 +0800

    实现类似cd工具的函数

---
+++ 04-file_operation/cd/chdir.c
@@ -0,0 +1,9 @@
+#include <43func.h>
+
+int main(int argc, char *argv[]) {
+    ARGS_CHECK(argc, 2);
+    printf("before chdir, cwd = %s\n", getcwd(NULL, 0));
+    int ret = chdir(argv[1]);
+    ERROR_CHECK(ret, -1, "chdir");
+    printf("after  chdir, cwd = %s\n", getcwd(NULL, 0));
+}
