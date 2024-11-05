580da4fac0ee1feb9a7c6ed99f25a93c8708184b
bhhh <3343806536@qq.com>
2024-11-05 17:41:56 +0800

    pipe全双工通信

--- 04-file_operation/pipe/chat1.c
+++ 04-file_operation/pipe/chat1.c
@@ -5,6 +5,15 @@ int main(int argc, char *argv[]) {
     ARGS_CHECK(argc, 3);
     int fdr = open(argv[1], O_RDONLY);
     int fdw = open(argv[2], O_WRONLY);
-    puts("pipe open");
-    
+    printf("I am chat1\n");
+    char buf[128] = {0};
+    while (1) {
+        memset(buf, 0, sizeof(buf));
+        read(STDIN_FILENO, buf, sizeof(buf));
+        write(fdw, buf, strlen(buf) - 1);
+        memset(buf, 0, sizeof(buf));
+        read(fdr, buf, sizeof(buf));
+        printf("buf = %s\n", buf);
+    }
+    return 0;
 }
--- 04-file_operation/pipe/chat2.c
+++ 04-file_operation/pipe/chat2.c
@@ -5,6 +5,15 @@ int main(int argc, char *argv[]) {
     ARGS_CHECK(argc, 3);
     int fdw = open(argv[1], O_WRONLY);
     int fdr = open(argv[2], O_RDONLY);
-    puts("pipe open");
-    
+    printf("I am chat2\n");
+    char buf[128] = {0};
+    while (1) {
+        memset(buf, 0, sizeof(buf));
+        read(fdr, buf, sizeof(buf));
+        printf("buf = %s\n", buf);
+        memset(buf, 0, sizeof(buf));
+        read(STDIN_FILENO, buf, sizeof(buf));
+        write(fdw, buf, strlen(buf) - 1);
+    }
+    return 0;
 }
