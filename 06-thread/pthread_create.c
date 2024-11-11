d0d79e22369447b67d5daf4a935fbf20de9e47fc
bhhh <3343806536@qq.com>
2024-11-11 16:38:47 +0800

    创建线程

--- 06-thread/pthread_create.c
+++ 06-thread/pthread_create.c
@@ -4,13 +4,13 @@
 
 void *threadFunc(void *arg) {
     printf("I am child thread, tid = %lu\n", pthread_self());
-    return NULL;
+    return NULL; // 多线程并发执行
 }
 
 int main() {
     pthread_t tid;
     int ret = pthread_create(&tid, NULL, threadFunc, NULL);
     printf("I am main thread, tid = %lu\n", pthread_self());
-    sleep(1);
+    sleep(1); // 如果这里取消sleep 结果只会打印主线程 主线程终止 整个线程也就终止了
     return 0;
 }
